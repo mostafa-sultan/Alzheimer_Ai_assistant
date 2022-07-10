1675ef5071c5559d3ae82e96ecf40f859f7475e2
mostafa-sultan <mostafasoltan82@gmail.com>
2022-07-10 12:47:16 +0200

    add googel map screen

--- App.js
+++ App.js
@@ -1,112 +1,97 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import { Provider } from 'react-redux';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// import { store } from './src/redux/store';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+// // import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { SafeAreaView, Text } from 'react-native';
+// import Adddevice from './src/screens/addDevice/AddDevice';
+// import Buy from './src/screens/buyDevice/Buy';
+// // import Home from './src/screens/home/Home';
+// import IntroSlider from './src/screens/introSlider/IntroSlider';
+// import Login from './src/screens/login/Login';
+import Mapview from './src/screens/map/MapView';
+// // import Detail from './src/screens/posts/detail';
+// // import Regsiter from './src/screens/register/Register';
+// import Tasks from './src/screens/tasks/Tasks';
+// import UserProfile from './src/screens/userProfile/UserProfile';
+// import Posts from './src/screens/posts/Posts';
+// import Register from './src/screens/register/Register';
+// import Reduxscreen from './src/screens/redux/ReduxScreen';
+// import Designkit from './src/screens/mykit/DesignKit';
+// // import Verify from './src/screens/verify/Verify';
+// import ResetPassword from './src/screens/resetPassword/ResetPassword';
+// // import Test from './src/screens/test/Test';
+// // import Devices from './src/screens/test/devices/Devices';
+// import Home from './src/screens/test/travel/screens/Home';
+// import Detail from './src/screens/test/travel/screens/Detail';
+// import Landing from './src/screens/test/land/Landing';
+// import Login2 from './src/screens/login/Login2';
+// import Regsiter from './src/screens/login/Register';
+// import Verify from './src/screens/login/Verify';
+// import Emptyhome from './src/screens/test/emptyHome/EmptyHome';
+// // import Ex from './src/screens/test/customDrwer/Ex';
+// // import Home from './src/screens/test/home/Home';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+// // import { createDrawerNavigator } from '@react-navigation/drawer';
+// // import { NavigationContainer } from '@react-navigation/native';
+// // const Stack = createNativeStackNavigator();
+// // const Drawer = createDrawerNavigator();
+
+const App = () => {
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    // // <Ex/>
+    // // <NavigationContainer>
+    // //   <Drawer.Navigator initialRouteName="Login2">
+    // //     <Drawer.Screen name="Login2" component={Login2} />
+    // //     <Drawer.Screen name="Home" component={Home} />
+    // //   </Drawer.Navigator>
+    // // </NavigationContainer>
+    // // <Detail/>
+    // // <Home/>
+    // // <Devices/>
+    // // <Home />
+    // //  <Test/>
+    // // <Landing/>
+    // // <Login2/>
+    // // <Regsiter/>
+    // // <Verify/>
+    // // <Emptyhome/>
+    // // <NavigationContainer>
+    // //   <Stack.Navigator initialRouteName="IntroSlider">
+    // //     <Stack.Screen options={{ headerShown: false }} name="IntroSlider" component={IntroSlider} />
+    // //     <Stack.Screen name="Login" component={Login} />
+    // //     <Stack.Screen name="Register" component={Register} />
+    // //     <Stack.Screen name="Verify" component={Verify} />
+    // //     <Stack.Screen name="ResetPassword" component={ResetPassword} /> 
+    // //     <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+    // //     <Stack.Screen  name="Detail" component={Detail} />
+    // //     <Stack.Screen name="Mapview" component={Mapview} />
+    // //   </Stack.Navigator>
+    // // </NavigationContainer>
+
+    // // <Designkit /> 
+    // // <IntroSlider/> 
+    // // <Provider store={store}>
+    // // <Reduxscreen/>
+    // // </Provider>
+    // // <Login />
+    // // <Verify/>
+    // // <ResetPassword/>
+    // // <Register/>
+    // // <Home />
+    // // <Adddevice/>
+    // // <Buy />
+    // // <Detail/>
+    <Mapview/>
+    // // <Posts/>
+    // // <Tasks/>
+    // // <UserProfile/>
+
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
